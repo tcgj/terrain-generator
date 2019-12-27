e0b58251d38e89eb88b8bf5b1c57c29f17e54021
tcgj <terenceblur0410@gmail.com>
2019-12-27 13:35:31 +0800

    Add player camera vertical rotation

--- Assets/Scripts/PlayerCameraController.cs
+++ Assets/Scripts/PlayerCameraController.cs
@@ -10,19 +10,23 @@ public class PlayerCameraController : MonoBehaviour {
     public float maxCameraHeightOffset = 4;
     public Transform playerBody;
 
-    float lateralRotation;
+    float lateralOffset;
+    float verticalRotation;
 
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update() {
-        playerBody.Rotate(Vector3.up * lateralRotation);
+        playerBody.Rotate(Vector3.up * lateralOffset);
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
     }
 
     public void OnLook(InputAction.CallbackContext context) {
         Vector2 lookPositionOnScreen = context.ReadValue<Vector2>();
 
-        lateralRotation = lookPositionOnScreen.x * mouseSensitivity * Time.deltaTime;
+        lateralOffset = lookPositionOnScreen.x * mouseSensitivity * Time.deltaTime;
+        verticalRotation -= lookPositionOnScreen.y * mouseSensitivity * Time.deltaTime;
+        verticalRotation = Mathf.Clamp(verticalRotation, -80f, 80f);
     }
 }
